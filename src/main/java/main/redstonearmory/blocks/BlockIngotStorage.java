8f03d3587da4395ee3300824a2c8980c127a50ec
Tombenpotter <thomas@benguigui.fr>
2014-06-19 10:49:13 +0200

    The block now shows again in the creative tab

--- src/main/java/main/redstonearmory/blocks/BlockIngotStorage.java
+++ src/main/java/main/redstonearmory/blocks/BlockIngotStorage.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import main.redstonearmory.RedstoneArmory;
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Icon;
 import redstonearsenal.block.BlockStorage;
 
@@ -12,28 +13,28 @@ import java.util.List;
 
 public class BlockIngotStorage extends BlockStorage {
 
-	@SideOnly(Side.CLIENT)
-	private Icon icon;
+    @SideOnly(Side.CLIENT)
+    private Icon icon;
 
-	public BlockIngotStorage(int id) {
-		super(id);
-		this.setCreativeTab(RedstoneArmory.tabRedstoneArmory);
-	}
+    public BlockIngotStorage(int id) {
+        super(id);
+        this.setCreativeTab(RedstoneArmory.tabRedstoneArmory);
+    }
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void registerIcons(IconRegister register) {
-		icon = register.registerIcon("redstonearmory:Block_GelidEnderium");
-	}
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IconRegister register) {
+        icon = register.registerIcon("redstonearmory:Block_GelidEnderium");
+    }
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public Icon getIcon(int side, int meta) {
-		return icon;
-	}
+    @Override
+    public void getSubBlocks(int block, CreativeTabs tab, List list) {
+        list.add(new ItemStack(block, 1, 0));
+    }
 
-	@Override
-	public void getSubBlocks(int ItemID, CreativeTabs tab, List list) {
-	
-	}
+    @SideOnly(Side.CLIENT)
+    @Override
+    public Icon getIcon(int side, int meta) {
+        return icon;
+    }
 }
