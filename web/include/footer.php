25c5bb9a35b0f2fc8484743c2fec2c9efbba7453
TehNut <tehnut@tehnut.com>
2014-09-25 22:49:51 -0700

    More changes due to site overhaul

--- web/include/footer.php
+++ web/include/footer.php
@@ -4,6 +4,7 @@
 	<div class="footer" style="clear: both; margin-top: 30px; height:60px; text-align: center; font-size:14px; color: rgb(150, 150, 150);">
     	<span>&copy; 2014 TehNut. Site originally designed by Nijikokun and Aesen for the <a href="http://www.spongepowered.org/" target="new">SpongePowered</a> project. I take no ownership of their work.</span><br>     
       	<span>Follow us on Twitter! <a href="https://twitter.com/OfficialTehNut" target="new">@OfficialTehNut</a> and <a href="https://twitter.com/Tombenpotter" target="new">@Tombenpotter</a></span><br />      
-      	<span>Header image by: <a href="http://www.reddit.com/user/simonsays476" target="new">/u/simonsays476</a></span>
+      	<span>Header image by: <a href="http://www.reddit.com/user/simonsays476" target="new">/u/simonsays476</a></span><br />
+        <span>Return to my homepage <a href="http://tehnut.info/">here</a></span>
 	</div>
 </div>
