5981c9a7d0ba27ecb71fc250ec6265471b08c9c3
TehNut <tehnut@tehnut.com>
2014-09-25 21:26:29 -0700

    Downloads are now on Jenkins

--- web/include/header.php
+++ web/include/header.php
@@ -5,7 +5,7 @@
           		<a href="../"><h1>Redstone Armory</h1></a>
           		<h2>Enhancing the (Redstone) Flux Revolution!</h2><br />
                 From the minds of TehNut and Tombenpotter.<br />
-          		<a href="../downloads/" class="btn btn-large btn-full"><span class="icon ion-android-download">  </span>Downloads</a>
+          		<a href="http://tehnut.info/jenkins/" class="btn btn-large btn-full"><span class="icon ion-android-download">  </span>Downloads</a>
             	<a href="../changelog/" class="btn btn-large btn-full"><span class="icon ion-ios7-paper">  </span>Changelog</a>
             	<a href="https://webchat.esper.net/?channels=tehnut" target="new" class="btn btn-large btn-empty"><span class="icon ion-pound">  </span>ChatWithUs</a>
             	<a href="http://www.minecraftforum.net/topic/2756729-" target="new" class="btn btn-large btn-full"><span class="icon ion-android-friends">  </span>MCF Post</a>
